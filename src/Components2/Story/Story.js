2d49bdbe69592f5ac886a57a24792eeff58414d3
Sahad <govardhan.n@codingmart.com>
2022-08-12 01:20:56 +0530

    commit

--- src/Components2/Story/Story.js
+++ src/Components2/Story/Story.js
@@ -18,7 +18,7 @@ function Story({stories}) {
                 <li>
                     <div className="position-relative my-3" style={{left:"0",marginRight:"15px",}}>
                         <div className="position-relative" style={{width: "60px", height:"60px", borderRadius:"50%", border: "2px solid #e84118", padding: "1px"}}>
-                        <img src={Img(story)} alt="..." className="img-fluid rounded-circle" />
+                        <img src={Img(story)} alt="..." draggable="false" className="img-fluid rounded-circle" style={{cursor:"pointer"}} />
                         </div>
                         <div className=" text-small mx-2"><small>{story.name}</small></div>
                     </div>
