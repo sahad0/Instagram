cd788c8e12600f62236da8a011e8d07699fe894e
Sahad <govardhan.n@codingmart.com>
2022-08-23 12:06:52 +0530

    commit

--- src/Components2/Story/Story.js
+++ src/Components2/Story/Story.js
@@ -10,9 +10,9 @@ function Story({ stories, setStories }) {
 
     function StoryView(story, id) {
         setModalImg(story);
-        if (modalImg) {
-            setOpen(true);
-        }
+
+        setOpen(true);
+
         const arr = [...stories];
 
         const update = arr.filter((k) => {
@@ -27,6 +27,10 @@ function Story({ stories, setStories }) {
 
 
 
+    }
+    function closeTheMod() {
+        setOpen(false);
+        setModalImg('');
     }
 
 
@@ -49,10 +53,10 @@ function Story({ stories, setStories }) {
                     </li>
 
 
-                    <div onClick={() => { setOpen(false) }} className={open ? "model open" : "model"} >
+                    <div onClick={closeTheMod} className={open ? "model open" : "model"} >
 
                         <img className='modal-img img-fluid' alt='picc' src={modalImg} />
-                        <svg onClick={() => { setOpen(false) }} aria-label="Close" style={{ position: "fixed", top: "10px", right: "10px" }} className="fg7vo5n6 lrzqjn8y m-5" color="#ffffff" fill="#ffffff" height="18" role="img" viewBox="0 0 48 48" width="18"><title>Close</title><path clipRule="evenodd" d="M41.8 9.8L27.5 24l14.2 14.2c.6.6.6 1.5 0 2.1l-1.4 1.4c-.6.6-1.5.6-2.1 0L24 27.5 9.8 41.8c-.6.6-1.5.6-2.1 0l-1.4-1.4c-.6-.6-.6-1.5 0-2.1L20.5 24 6.2 9.8c-.6-.6-.6-1.5 0-2.1l1.4-1.4c.6-.6 1.5-.6 2.1 0L24 20.5 38.3 6.2c.6-.6 1.5-.6 2.1 0l1.4 1.4c.6.6.6 1.6 0 2.2z" fillRule="evenodd"></path></svg>
+                        <svg onClick={closeTheMod} aria-label="Close" style={{ position: "fixed", top: "10px", right: "10px" }} className="fg7vo5n6 lrzqjn8y m-5" color="#ffffff" fill="#ffffff" height="18" role="img" viewBox="0 0 48 48" width="18"><title>Close</title><path clipRule="evenodd" d="M41.8 9.8L27.5 24l14.2 14.2c.6.6.6 1.5 0 2.1l-1.4 1.4c-.6.6-1.5.6-2.1 0L24 27.5 9.8 41.8c-.6.6-1.5.6-2.1 0l-1.4-1.4c-.6-.6-.6-1.5 0-2.1L20.5 24 6.2 9.8c-.6-.6-.6-1.5 0-2.1l1.4-1.4c.6-.6 1.5-.6 2.1 0L24 20.5 38.3 6.2c.6-.6 1.5-.6 2.1 0l1.4 1.4c.6.6.6 1.6 0 2.2z" fillRule="evenodd"></path></svg>
                     </div>
                 </div>
 
